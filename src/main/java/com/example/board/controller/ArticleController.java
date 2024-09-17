7644e4a36d4dd39047d4e72e555941afe855f513
dobby <tmakdlf2482@naver.com>
2024-09-17 19:34:03 +0900

    update article

--- src/main/java/com/example/board/controller/ArticleController.java
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -50,8 +50,10 @@ public class ArticleController {
 
         // 1. id로 데이터를 가져옴
         Article articleEntity = articleRepository.findById(id).orElse(null); // .orElse(null)은 만약 id를 못찾았다면 null을 반환
+
         // 2. 가져온 데이터를 모델에 등록
         model.addAttribute("article", articleEntity);
+
         // 3. 보여줄 페이지를 설정
         return "articles/show"; // articles/show.mustache -> 브라우저로 전송!
     }
@@ -60,10 +62,45 @@ public class ArticleController {
     public String index(Model model) {
         // 1. 모든 Article을 가져옴
         List<Article> articleEntityList = articleRepository.findAll(); // articleRepository에 해당하는 모든 데이터를 전부 가져옴, ArrayList 상위 타입이 List임 (업캐스팅)
+
         // 2. 가져온 Article 묶음을 뷰로 전달함 (모델에 등록)
         model.addAttribute("articleList", articleEntityList);
+
         // 3. 보여줄 페이지를 설정
         return "articles/index"; // articles/index.mustache -> 브라우저로 전송!
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) { // @PathVariable Long id 라고 하면 URL의 id값을 가져옴, 예를 들어 /articles/1/edit이면 1을 가져오고, /articles/2/edit이면 2를 가져옴
+        // 1. 수정할 데이터를 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null); // .orElse(null)은 만약 id를 못찾았다면 null을 반환
+
+        // 2. 가져온 article을 모델에 데이터를 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3. 뷰 페이지 설정
+        return "articles/edit"; // articles/edit.mustache -> 브라우저로 전송!
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        // log.info(form.toString());
+
+        // 1. DTO를 Entity로 변환!
+        Article articleEntity = form.toEntity();
+        // log.info(articleEntity.toString());
+
+        // 2. Repository(실제 DB에 저장하게 하는 역할)에게 Entity를 DB안에 저장하게 함!
+        // 2-1. DB에서 기존 데이터를 가져옴
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null); // articleRepository가 id값을 통해 데이터를 가져오는데 만약에 없다면 null을 return
+        // log.info(target.toString());
+        // 2-2. 기존 데이터에 값을 갱신함
+        if (target != null) { // 기존 데이터가 있다면
+            articleRepository.save(articleEntity); // 값을 갱신, entity가 DB로 갱신됨
+        }
+
+        // 3. 수정 결과 페이지로 redirect 함
+        return "redirect:/articles/" + articleEntity.getId();
     }
 
 }
--- src/main/java/com/example/board/dto/ArticleForm.java
+++ src/main/java/com/example/board/dto/ArticleForm.java
@@ -9,12 +9,13 @@ import lombok.ToString;
 // new.mustache에서 form 데이터를 받아올 그릇
 public class ArticleForm {
 
+    private Long id; // id 필드 추가!! (edit.mustache에 name="id"가 추가되었으므로 dto에도 id 필드 추가해줘야함)
     private String title;
     private String content;
 
     // DTO인 form에서 Entity객체로 변환하는 메소드
     public Article toEntity() {
-        return new Article(null, title, content); // Entity(Article) 클래스의 객체를 생성해야 하므로 생성자 호출
+        return new Article(id, title, content); // Entity(Article) 클래스의 객체를 생성해야 하므로 생성자 호출
     }
 
 }
