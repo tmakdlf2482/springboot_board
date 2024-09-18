97ddd4c31502ce7537ce08ec1ead8295208f5844
dobby <tmakdlf2482@naver.com>
2024-09-18 10:28:57 +0900

    DB setting

--- src/main/java/com/example/board/entity/Article.java
+++ src/main/java/com/example/board/entity/Article.java
@@ -1,23 +1,20 @@
 package com.example.board.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity // DB가 해당 객체(Entity)를 인식 가능!
+@Entity // DB가 해당 객체(Entity)를 인식 가능! (실제로 해당 클래스로 테이블을 만들어 줌)
 @AllArgsConstructor
 @NoArgsConstructor // 디폴트 생성자를 추가해줌!
 @ToString
 @Getter
 public class Article {
 
-    @Id
-    @GeneratedValue // 자동 생성 어노테이션! (1, 2, 3, ...)
+    @Id // 대표값(PK)을 지정! (주민등록번호 같은거)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id를 자동 생성 어노테이션!, 겹치지 않게 자동으로 넣어줌
     private Long id; // 기본키
 
     @Column
